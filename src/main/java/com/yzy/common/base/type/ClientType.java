79938c396a318ca892c3fcb362f004c086ef575b
江海洋 <1109377497@qq.com>
2018-07-04 17:00:28 +0800

    buchong

---
+++ src/main/java/com/yzy/common/base/type/ClientType.java
@@ -0,0 +1,9 @@
+package com.yzy.common.base.type;
+
+/**
+ * @Description:
+ * @Author: Jhy
+ * @Date: 2018-07-04 
+ */
+public class ClientType {
+}
---
+++ src/main/java/com/yzy/common/base/type/MsgType.java
@@ -0,0 +1,11 @@
+package com.yzy.common.base.type;
+
+
+/**
+ * @Description:消息类型
+ * @Author: Jhy
+ * @Date: 2018-07-04 
+ */
+public enum  MsgType {
+    PING,LOGIN,PUSH;
+}
